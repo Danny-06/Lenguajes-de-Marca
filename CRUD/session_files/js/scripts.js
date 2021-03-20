dd2efe4d1e2b4d08b83a3eba6d9246b322ef1a89
Daniel Fabián <72435753+Danny-06@users.noreply.github.com>
2021-03-20 21:10:15 +0100

    Update scripts.js

--- CRUD/session_files/js/scripts.js
+++ CRUD/session_files/js/scripts.js
@@ -11,7 +11,7 @@ let toggle = 0;
 // Si es un usuario de movil,
 // ocultar el panel lateral y
 // ajustar variable de la palanca
-if(navigator.userAgent.includes("Mobile")) {
+if(navigator.userAgent.includes("Mobile") || window.innerWidth <= 1050) {
   main.setAttribute('style', '--mainContentWidth: 100%;')
   toggle++;
 }
