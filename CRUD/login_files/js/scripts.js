94a3cb0a4e687350685fd7b74c7e741560362f80
Daniel Fabián <72435753+Danny-06@users.noreply.github.com>
2021-05-27 23:03:27 +0200

    Add files via upload

---
+++ CRUD/login_files/js/scripts.js
@@ -0,0 +1,67 @@
+const form = document.querySelector('.form');
+
+const emailInput = form.querySelector('.user');
+const emailDetails = form.querySelector('.user + .details');
+
+const passwordInput = form.querySelector('.password');
+const passwordDetails = form.querySelector('.password + .details');
+
+
+const submitInput = form.querySelector('.submit');
+
+
+submitInput.addEventListener('click', ValidarLogin);
+form.addEventListener("keydown", desmarcarError);
+
+
+
+
+
+
+
+
+// Funciones
+
+function ValidarLogin() {
+
+  if( !esCorreoValido() ) {
+    event.preventDefault();
+    emailInput.classList.add('error');
+    emailDetails.classList.add('error');
+
+    return false;
+  }
+
+  if( !esContrasenaValida() ) {
+    event.preventDefault();
+    passwordInput.classList.add('error');
+    passwordDetails.classList.add('error');
+
+    return false;
+  }
+
+  return true;
+
+}
+
+function desmarcarError() {
+  emailInput.classList.remove('error');
+  emailDetails.classList.remove('error');
+
+  passwordInput.classList.remove('error');
+  passwordDetails.classList.remove('error');
+}
+
+
+const regexEmail = /^[a-zñA-ZÑ1-9\.]+@[a-z1-9]+\.[a-z]{2,4}$/;
+
+function esCorreoValido() {
+  return regexEmail.test(emailInput.value);
+}
+
+
+const regexPassword = /^(?=.*?[A-ZÑ])(?=.*?[a-zñ])(?=.*?[#&@!%$\-*]).{6,20}/;
+
+function esContrasenaValida() {
+  return regexPassword.test(passwordInput.value);
+}
